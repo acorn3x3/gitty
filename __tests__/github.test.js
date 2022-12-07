ea51778be4a39fe9bbea9a10fe94f7dd049a4a3d
Andrew Cornejo <atcornejo3@gmail.com>
2022-12-06 17:17:34 -0800

    created test for login users routed to dashboard

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -9,14 +9,30 @@ describe('github auth', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  afterAll(() => {
-    pool.end();
-  });
 
-  it('api/v1/github/login should redirect to the github oauth page', async () => {
+  it('/api/v1/github/login should redirect to the github oauth page', async () => {
     const resp = await request(app).get('/api/v1/github/login');
     expect(resp.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
     );
   });
+});
+
+it('/api/v1/github/callback should login users to the dashboard', async () => {
+  const resp = await request
+    .agent(app)
+    .get('/api/v1/github/callback?code=42')
+    .redirects(1);
+  console.log(resp.body);
+  expect(resp.body).toEqual({
+    id: expect.any(String),
+    login: 'fake_github_user',
+    email: 'not-real@example.com',
+    avatar: expect.any(String),
+    iat: expect.any(Number),
+    exp: expect.any(Number),
+  });
+});
+afterAll(() => {
+  pool.end();
 });
